898d28c0a6a7c54831f2f9e66fe1c928dcef0c75
Piotr Ferens <piotrferens@outlook.com>
2021-08-31 12:13:04 +0200

    Add basic progress bar and spinner when page is loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Head from 'next/head';
 
 import { AppProviders } from 'src/AppProviders';
 import { defaultPageMeta } from 'src/helpers/createPage/createPage';
 import { CreatePageComponent } from 'src/helpers/createPage/createPage.types';
 
 export type OwnAppProps = AppProps & { Component: CreatePageComponent };
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }: OwnAppProps) => {
   const pageMeta = Component.meta || defaultPageMeta;
@@ -23,7 +30,14 @@ const MyApp = ({ Component, pageProps }: OwnAppProps) => {
     }
   }, []);
 
-  return <AppProviders>{page}</AppProviders>;
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      <AppProviders>{page}</AppProviders>
+    </>
+  );
 };
 
 export default MyApp;
